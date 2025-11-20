8118beb515f0fd8ca19e2a86e6016c33c10fc05e
Ryan Coe <ryan@ryco.dev>
2025-11-20 07:44:29 -0800

    add support for php 8.5.0
    Signed-off-by: Ryan Coe <ryan@ryco.dev>

--- mosquitto.c
+++ mosquitto.c
@@ -1249,7 +1249,11 @@ PHP_MINIT_FUNCTION(mosquitto)
 	mosquitto_ce_client->create_object = mosquitto_client_object_new;
 
 	INIT_NS_CLASS_ENTRY(exception_ce, "Mosquitto", "Exception", NULL);
+#if PHP_VERSION_ID >= 80500
+	mosquitto_ce_exception = zend_register_internal_class_ex(&exception_ce, zend_ce_exception);
+#else
 	mosquitto_ce_exception = zend_register_internal_class_ex(&exception_ce, zend_exception_get_default());
+#endif
 
 	#define REGISTER_MOSQUITTO_LONG_CONST(const_name, value) \
 	zend_declare_class_constant_long(mosquitto_ce_client, const_name, sizeof(const_name)-1, (long)value); \
